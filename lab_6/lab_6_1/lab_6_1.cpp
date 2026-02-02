6e26510a5c7424dfd7a07aad95aab97b2c6ff7cc
yukoma <kevin11000@mail.ru>
2026-02-02 14:35:25 +0300

    added lab_6_1

---
+++ lab_6/lab_6_1/lab_6_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <coroutine>
+#include <iostream>
+
+struct promise;
+
+struct coroutine : std::coroutine_handle<promise>
+{
+    using promise_type = ::promise;
+};
+
+struct promise
+{
+    coroutine get_return_object() { return { coroutine::from_promise(*this) }; }
+    std::suspend_always initial_suspend() noexcept { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
+    void return_void() {}
+    void unhandled_exception() {}
+};
+
+int main()
+{
+    coroutine h = [](int i) -> coroutine
+        {
+            std::cout << i;
+            co_return;
+        }(4026);
+    h.resume();
+    h.destroy();
+    return 0;
+}
